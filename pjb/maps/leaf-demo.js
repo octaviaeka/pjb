7b9bce7ef2863fc0f78d272214041f3a7d3c50b1
octaviaeka <60868303+octaviaeka@users.noreply.github.com>
2020-06-17 17:11:52 +0700

    Add files via upload

---
+++ pjb/maps/leaf-demo.js
@@ -0,0 +1,27 @@
+var map = L.map( 'map', {
+  center: [-7.250445, 112.768845],
+  minZoom: 2,
+  zoom: 2
+})
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo( map )
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' )
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+})
+
+for ( var i=0; i < markers.length; ++i )
+{
+ L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+  .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+  .addTo( map );
+}
