6080718722079b0e90da2d96a725364c5e26dd00
octaviaeka <60868303+octaviaeka@users.noreply.github.com>
2020-06-19 22:23:22 +0700

    Add files via upload

---
+++ pjb2/src/markers.js
@@ -0,0 +1,26 @@
+var markers = [
+    {
+      "name": "Surabaya",
+      "url": "https://en.wikipedia.org/wiki/Canada",
+      "lat": -7.250445,
+      "lng":  112.768845
+    },
+    {
+      "name": "Tulungagung",
+      "url": "https://en.wikipedia.org/wiki/Anguilla",
+      "lat": -8.0658642,
+      "lng": 111.9026111
+    },
+    {
+      "name": "Batu",
+      "url": "https://en.wikipedia.org/wiki/Barbados",
+      "lat": -7.8671,
+      "lng": 112.5239
+    },
+    {
+      "name": "Bandung",
+      "url": "https://en.wikipedia.org/wiki/United_States",
+      "lat": -6.917464,
+      "lng": 107.619123
+    }
+ ];
