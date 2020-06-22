bd9320cf2c78f572746264b12bd8bbe423a5d776
octaviaeka <60868303+octaviaeka@users.noreply.github.com>
2020-06-22 09:40:13 +0700

    Update markers.js

--- pjb2/src/markers.js
+++ pjb2/src/markers.js
@@ -1,26 +1,30 @@
 var markers = [
-    {
-      "name": "Surabaya",
-      "url": "https://en.wikipedia.org/wiki/Canada",
-      "lat": -7.250445,
-      "lng":  112.768845
-    },
-    {
-      "name": "Tulungagung",
-      "url": "https://en.wikipedia.org/wiki/Anguilla",
-      "lat": -8.0658642,
-      "lng": 111.9026111
-    },
-    {
-      "name": "Batu",
-      "url": "https://en.wikipedia.org/wiki/Barbados",
-      "lat": -7.8671,
-      "lng": 112.5239
-    },
-    {
-      "name": "Bandung",
-      "url": "https://en.wikipedia.org/wiki/United_States",
-      "lat": -6.917464,
-      "lng": 107.619123
-    }
- ];
+  {
+    "id" : 1,
+    "name": "Surabaya",
+    "lat": -7.250445,
+    "lng":  112.768845,
+    "kapasitas": "5.068 MW"
+  },
+  {
+    "id"  : 2,
+    "name": "Tulungagung",
+    "lat": -8.0658642,
+    "lng": 111.9026111,
+    "kapasitas": "1.008 MW"
+  },
+  {
+    "id"  : 3,
+    "name": "Batu",
+    "lat": -7.8671,
+    "lng": 112.5239,
+    "kapasitas": "3.068 MW"
+  },
+  {
+    "id"  : 4,
+    "name": "Bandung",
+    "lat": -6.917464,
+    "lng": 107.619123,
+    "kapasitas": "5.068 MW"
+  }
+];
