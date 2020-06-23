3d342352cd483b22cbf230b003e68e8ffd25d061
octaviaeka <60868303+octaviaeka@users.noreply.github.com>
2020-06-23 19:19:30 +0700

    Add files via upload

---
+++ pjb3/src/markers.js
@@ -0,0 +1,30 @@
+var markers = [
+  {
+    "id" : 1,
+    "name": "Surabaya",
+    "lat": -7.250445,
+    "lng":  112.768845,
+    "kapasitas": "5.068 MW"
+  },
+  {
+    "id"  : 2,
+    "name": "Tulungagung",
+    "lat": -8.0658642,
+    "lng": 111.9026111,
+    "kapasitas": "1.008 MW"
+  },
+  {
+    "id"  : 3,
+    "name": "Batu",
+    "lat": -7.8671,
+    "lng": 112.5239,
+    "kapasitas": "3.068 MW"
+  },
+  {
+    "id"  : 4,
+    "name": "Bandung",
+    "lat": -6.917464,
+    "lng": 107.619123,
+    "kapasitas": "5.068 MW"
+  }
+];
